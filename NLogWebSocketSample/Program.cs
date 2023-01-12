2c0ef73709f7457ec7ecfb9d8c326318795c2c9c
@guanghao.ma <guanghao.ma@ytever.com>
2023-01-12 15:17:04 +0800

    添加项目文件

---
+++ NLogWebSocketSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+
+namespace NLogWebSocketSample
+{
+    internal class Program
+    {
+        static readonly Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            while (true)
+            {
+                logger.Info($"随机数：{random.Next()}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
